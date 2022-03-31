998ea56ff11594269730613af63ec9f96f7156e7
kelvingcr <contato@kelvindev.tk>
2022-03-31 20:20:45 -0300

    Navegação criada

--- app/src/main/java/br/com/kelvingcr/netflix/activity/MainActivity.java
+++ app/src/main/java/br/com/kelvingcr/netflix/activity/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.kelvingcr.netflix.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import br.com.kelvingcr.netflix.R;
 
@@ -12,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
--- app/src/main/java/br/com/kelvingcr/netflix/activity/SplashActivity.java
+++ app/src/main/java/br/com/kelvingcr/netflix/activity/SplashActivity.java
@@ -2,7 +2,10 @@ package br.com.kelvingcr.netflix.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 
 import br.com.kelvingcr.netflix.R;
@@ -13,6 +16,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() ->{
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }, 3000);
 
     }
     @Override
