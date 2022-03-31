f0599838be24114b987376cf0135006ba6b630a0
kelvingcr <contato@kelvindev.tk>
2022-03-31 20:47:36 -0300

    Criada a tela de Login

---
+++ app/src/main/java/br/com/kelvingcr/netflix/autenticacao/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.kelvingcr.netflix.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.kelvingcr.netflix.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
