809da7c3c5ea2ce54480589998ff8541c4d594e6
kelvingcr <contato@kelvindev.tk>
2022-04-01 11:01:17 -0300

    Tela de cadastro de usuario criada

--- app/src/main/java/br/com/kelvingcr/netflix/autenticacao/CadastroActivity.java
+++ app/src/main/java/br/com/kelvingcr/netflix/autenticacao/CadastroActivity.java
@@ -7,18 +7,19 @@ import android.os.Bundle;
 
 import br.com.kelvingcr.netflix.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_cadastro);
+
         configCliques();
     }
 
     private void configCliques(){
-        findViewById(R.id.btn_cadastro).setOnClickListener(view -> {
-            startActivity(new Intent(this, CadastroActivity.class));
+        findViewById(R.id.btn_entrar).setOnClickListener(view -> {
+            finish();
         });
     }
 }
